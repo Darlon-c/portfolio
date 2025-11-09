56eb40b2f9bd926094ff245b9da7551f3161bc6c
Darlon <darloncosta050@gmail.com>
2025-11-09 18:07:58 -0300

    atualizando paginas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./**/*.html",           // Todos os HTML na pasta raiz e subpastas
-    "./*.html",              // HTML na pasta raiz
-    "./src/**/*.{js,ts,jsx,tsx}", // Arquivos JS/TS na pasta src
+   content: [
+    "./*.html",
+    "./pages/*.html",
+    "./**/*.html"
   ],
   theme: {
     extend: {
